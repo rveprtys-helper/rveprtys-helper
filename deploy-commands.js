8cc992abad889e062d4755daaedc70b5ad08f065
rveprtys-helper <jjog850@gmail.com>
2026-04-25 13:03:56 -0500

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+
+const commands = [];
+const files = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
+
+for (const file of files) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    await rest.put(
+        Routes.applicationGuildCommands(
+            process.env.CLIENT_ID,
+            process.env.GUILD_ID
+        ),
+        { body: commands }
+    );
+
+    console.log('Commands deployed');
+})();
