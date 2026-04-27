c406221591f7d5a4df9b1deb43f8463aaaefcd03
rveprtys-helper <jjog850@gmail.com>
2026-04-27 18:07:04 -0500

    Create ogive.js

---
+++ commands/ogive.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getUser } = require('../utils/economy');
+
+const OWNER_ID = "1453078748080504996";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ogive')
+        .setDescription('Owner give money')
+        .addUserOption(o => o.setName('user').setRequired(true))
+        .addIntegerOption(o => o.setName('amount').setRequired(true)),
+
+    async execute(interaction) {
+        if (interaction.user.id !== OWNER_ID) return;
+
+        const target = interaction.options.getUser('user');
+        const amount = interaction.options.getInteger('amount');
+
+        const user = await getUser(target.id, interaction.guild.id);
+        user.balance += amount;
+
+        await user.save();
+
+        interaction.reply(`Gave $${amount} to ${target.username}`);
+    }
+};
